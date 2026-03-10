68c4e29827f498c2aba6ad2d23d51402b49ee59d
SheharyarAli-Dev <sheharyar.ali.job@gmail.com>
2026-03-10 21:39:05 +0500

    Series Array implementation {Label:Value}

--- Pandas/PandasPractice.py
+++ Pandas/PandasPractice.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import pandas as pd;
+
+a=pd.Series([1,2,3], index=["a","b","c"])
+
+print(a)
+print(a["a"])
+
+b=pd.Series([4,5,6], index=["d","e","f"]);
+
+print(b);
+print(b["e"])
