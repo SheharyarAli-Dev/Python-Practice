0cd8ec8049cd21dcff03d595eab9024f330e919e
SheharyarAli-Dev <sheharyar.ali.job@gmail.com>
2026-03-10 22:13:44 +0500

    Further practice, handled series

--- Pandas/PandasPractice.py
+++ Pandas/PandasPractice.py
@@ -1,3 +1,5 @@
+#TOPIC 1
+"""
 import pandas as pd;
 
 a=pd.Series([1,2,3], index=["a","b","c"])
@@ -9,3 +11,29 @@ b=pd.Series([4,5,6], index=["d","e","f"]);
 
 print(b);
 print(b["e"])
+
+
+c={
+    "a":1,
+    "b":2,
+    "c":3
+}
+
+d=pd.Series(c)
+print(d[["a","c"]])
+print(b[["d","f"]])
+"""
+#TOPIC 2
+
+import pandas as pd
+
+a=pd.Series([1,2,3], index=["a","b","c"], name="Scores");
+
+print(a);
+print(a["a"])
+print(a[["a","c"]])
+print(a.index)
+print(a.values)
+print(a.dtype)
+print(a.name)
+
